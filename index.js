a08efa5d5098c7e5d3bc125dc9adef6f7a264159
stanleyndonj <stanley.ndonj@student.moringaschool.com>
2024-07-13 22:46:28 +0300

    lab completed

---
+++ index.js
@@ -0,0 +1,15 @@
+// Define the divide function as a function expression
+const divide = function() {
+    return 2000 / 100;
+  };
+  
+  // Define the square function as an arrow function
+  const square = (x) => {
+    return x * x;
+  };
+  
+  // Define the add function as an arrow function
+  const add = (a, b) => {
+    return a + b;
+  };
+  
